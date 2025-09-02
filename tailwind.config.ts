b64403a2aa69197ca76cdc4222f1deca03a27fe9
Shadeer <shadeer341@gmail.com>
2025-09-02 05:49:13 +0000

    lets use this font
    <link rel="preconnect" href="https://fonts.googleapi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Lexend', 'sans-serif'],
+        headline: ['Lexend', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
