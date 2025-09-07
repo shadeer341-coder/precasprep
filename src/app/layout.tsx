9cb18debc090342d0cd1c9626bf8f2b4d253080d
Shadeer <shadeer341@gmail.com>
2025-09-07 14:34:43 +0000

    lets make it start with light theme instead of dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
